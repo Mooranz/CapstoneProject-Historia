eabdc06d3f2a1546f32eee5fdf0bdc238608ac04
Mooranz <muhammadzhafran18mei@gmail.com>
2024-05-31 21:11:40 +0700

    DetailActivity where the source is from resource string

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,13 +50,5 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
-    implementation("com.squareup.retrofit2:retrofit:2.11.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
-
     implementation("com.github.bumptech.glide:glide:4.16.0")
-
-    //Untuk mengedit isi html
-    implementation("org.jsoup:jsoup:1.17.2")
-
 }
--- app/src/main/java/com/tugas/capstoneproject_historia/DetailActivity.kt
+++ app/src/main/java/com/tugas/capstoneproject_historia/DetailActivity.kt
@@ -6,48 +6,32 @@ import android.os.Bundle
 import android.text.Html
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.tugas.capstoneproject_historia.databinding.ActivityDetailBinding
 
-
 class DetailActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDetailBinding
-    var clicked = false
+    private var clicked = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //enableEdgeToEdge()
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // karena tidak terdapat di wikipiedia api
-        binding.tvLocation.visibility = View.GONE
-        binding.tvYearMade.visibility = View.GONE
-
         val actionBar = supportActionBar
-        actionBar?.setDisplayHomeAsUpEnabled(true);
+        actionBar?.setDisplayHomeAsUpEnabled(true)
 
-        DetailViewModel().getData()
+        val imgLink = getString(R.string.tugu_img)
+        val detailText = getString(R.string.tugu_en_detail)
 
-        DetailViewModel().data.observe(this) {
-            val imgLink = it.query.pages.jsonMemberPage.thumbnail.source
-            var detailText = it.query.pages.jsonMemberPage.extract
-            detailText = DetailViewModel().removeReferencesAndExternalLinks(detailText)
+        binding.tvTitle.text = getString(R.string.tugu_title)
+        binding.tvDetails.text = Html.fromHtml(detailText, Html.FROM_HTML_MODE_COMPACT)
+        binding.tvLink.text = getString(R.string.tugu_link)
 
-            binding.tvTitle.text = it.query.normalized.first().to.toString()
-            binding.tvDetails.text = Html.fromHtml(detailText, Html.FROM_HTML_MODE_COMPACT)
-            binding.tvLink.text = it.query.pages.jsonMemberPage.fullurl
-
-            Glide.with(this)
-                .load(imgLink)
-                .into(binding.imgDetail)
-        }
-
-        DetailViewModel().isLoading.observe(this) {
-            showLoading(it)
-        }
+        Glide.with(this)
+            .load(imgLink)
+            .into(binding.imgDetail)
 
         if (clicked == false) {
             binding.tvLink.setTextColor(getColor(com.bumptech.glide.R.color.material_blue_grey_800))
@@ -62,10 +46,6 @@ class DetailActivity : AppCompatActivity() {
             binding.tvLink.setTextColor(getColor(com.bumptech.glide.R.color.abc_tint_default))
 
         }
-    }
-
-    private fun showLoading(isLoading: Boolean) {
-        binding.progressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
