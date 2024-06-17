6b28a1205645ddfd578d31c5b0188c4f43ae1182
Mooranz <muhammadzhafran18mei@gmail.com>
2024-06-17 08:02:35 +0700

    Adding maps pins

--- app/src/main/java/com/tugas/capstoneproject_historia/ui/achievement/MapsActivity.kt
+++ app/src/main/java/com/tugas/capstoneproject_historia/ui/achievement/MapsActivity.kt
@@ -1,13 +1,17 @@
 package com.tugas.capstoneproject_historia.ui.achievement
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.MarkerOptions
 import com.tugas.capstoneproject_historia.R
 import com.tugas.capstoneproject_historia.databinding.ActivityMapsBinding
@@ -16,6 +20,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+
+    private val boundsBuilder = LatLngBounds.Builder()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,9 +48,65 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        mMap.uiSettings.apply {
+            isMapToolbarEnabled = true
+            isCompassEnabled = true
+            isZoomControlsEnabled = true
+            setAllGesturesEnabled(true)
+        }
+
+        getMyLocation()
+        addManyMarker()
+    }
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                getMyLocation()
+            }
+        }
+
+    private fun getMyLocation() {
+        if (ContextCompat.checkSelfPermission(
+                this.applicationContext,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            mMap.isMyLocationEnabled = true
+        } else {
+            requestPermissionLauncher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+    }
+
+    data class HistoricalPlace(
+        val name: String,
+        val latitude: Double,
+        val longitude: Double
+    )
+    private fun addManyMarker() {
+        val historicalPlaces = listOf(
+            HistoricalPlace("Tugu Yogyakarta", -7.782620149814713, 110.36712224478335),
+            HistoricalPlace("Monumen Jogja Kembali", -7.749434596214469, 110.3695530723198),
+            HistoricalPlace("Candi Ratu Boko", -7.770492710652259, 110.48938804478968),
+            HistoricalPlace("Candi Prambanan", -7.7516983924192395, 110.4914599356404),
+            HistoricalPlace("Benteng Vredeburg", -7.800183459357497, 110.36619670087443),
+        )
+        historicalPlaces.forEach { tourism ->
+            val latlng = LatLng(tourism.latitude, tourism.longitude)
+            mMap.addMarker(MarkerOptions().position(latlng).title(tourism.name))
+            boundsBuilder.include(latlng)
+        }
+
+        val bounds: LatLngBounds = boundsBuilder.build()
+        mMap.animateCamera(
+            CameraUpdateFactory.newLatLngBounds(
+                bounds,
+                resources.displayMetrics.widthPixels,
+                resources.displayMetrics.heightPixels,
+                300
+            )
+        )
     }
 }
